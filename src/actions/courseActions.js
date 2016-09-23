426d73a309dbd96bc0a9aeb3020c55c41c23188e
RSkendzic <rskendzic@execom.eu>
2016-09-23 09:09:18 +0200

    async in redux

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,6 +1,20 @@
 import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
 
-export function createCourse(course)
+export function loadCoursesSuccess(course)
 {
-  return { type: types.CREATE_COURSE, course};
+  return { type: types.LOAD_COURSES_SUCCESS, course};
+}
+
+export function loadCourses()
+{
+  return function(dispatch) {
+    return courseApi.getAllCourses().then(courses =>
+    {
+      dispatch(loadCoursesSuccess(courses));
+    }).catch(error =>
+    {
+      throw(error);
+    });
+  };
 }
--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -7,24 +7,24 @@ class CoursesPage extends React.Component{
 
   constructor(props,context){
     super(props,context);
-    this.state =
-    {
-    course: {title: " "}
-    };
-
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
+    // this.state =
+    // {
+    // course: {title: " "}
+    // };
+    //
+    // this.onTitleChange = this.onTitleChange.bind(this);
+    // this.onClickSave = this.onClickSave.bind(this);
   }
 
-  onTitleChange(event){
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({course: course});
-  }
+  // onTitleChange(event){
+  //   const course = this.state.course;
+  //   course.title = event.target.value;
+  //   this.setState({course: course});
+  // }
 
-  onClickSave(){
-    this.props.actions.createCourse(this.state.course);
-  }
+  // onClickSave(){
+  //   this.props.actions.createCourse(this.state.course);
+  // }
 
   courseRow(course,index)
   {
@@ -38,17 +38,16 @@ class CoursesPage extends React.Component{
           Courses
         </h1>
         <h3>{this.props.courses.map(this.courseRow)}</h3>
-        <h2>Add Course</h2>
+        {/* <h2>Add Course</h2>
         <input
-        type="text"
-        onChange={this.onTitleChange}
-        value={this.state.course.title}/>
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title}/>
 
         <input
-        type="submit"
-        onClick={this.onClickSave}
-        value="Save"/>
-
+          type="submit"
+          onClick={this.onClickSave}
+          value="Save"/> */}
       </div>
     );
   }
