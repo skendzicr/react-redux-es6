b2ba9e0f8c2e4c6e9104c3c9213ef3e8cf4b5eef
RSkendzic <rskendzic@execom.eu>
2016-09-23 10:20:03 +0200

    Async in Redux done

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react';
 import {connect} from 'react-redux';
 import * as courseActions from "../../actions/courseActions";
 import {bindActionCreators} from 'redux';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component{
 
@@ -32,12 +33,14 @@ class CoursesPage extends React.Component{
   }
 
   render () {
+    const {courses}= this.props;
+
     return (
       <div>
         <h1>
           Courses
         </h1>
-        <h3>{this.props.courses.map(this.courseRow)}</h3>
+        <CourseList courses={courses} />
         {/* <h2>Add Course</h2>
         <input
           type="text"
